71c64c414b275079788c590d1f103b4cd10e3cb9
Tuyen Ha <tuyen_ha@outlook.com>
2022-06-08 12:50:41 -0600

    commit tree cc902c67a3e95b428e5cca4dac7ed465651fd1f7

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const winYear = record.find(record => record.result === "W");
+    if (winYear) {
+        return winYear.year;
+    } else {
+        return undefined;
+    }
+}
